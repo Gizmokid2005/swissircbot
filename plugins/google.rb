369d2fefa4d486d26bbd8c80a9f171b94ef17592
Gizmokid2005 <gizmokid2005@gmail.com>
2014-06-30 11:37:58 -0400

    Google plugin - Cleaned up the code a bit for readability. Cleansed the results so we gather useful links instead of google search-laced links.

--- plugins/google.rb
+++ plugins/google.rb
@@ -8,13 +8,21 @@ class Google
   match /google (.+)/i
 
   def search(query)
-    url = "http://www.google.com/search?q=#{CGI.escape(query)}"
-    res = Nokogiri.parse(open(url.to_s).read).at("h3.r")
 
-    title = res.text
-    link = res.at('a')[:href].gsub("/url?q=","")
-    desc = res.at("./following::div").children.first.text
-    CGI.unescape_html "#{title} - #{desc} (#{link})"
+    url = "http://www.google.com/search?q=#{CGI.escape(query)}"
+
+    res = Nokogiri.parse(open(url.to_s).read).at("h3.r")
+    if res.text
+      title = res.text
+    else
+      title = "Unable to find title"
+    end
+    if res.at('a')[:href]
+      link = res.at('a')[:href].split("/url?q=").last.split("&").first
+    else
+      link = "Unable to parse link"
+    end
+    CGI.unescape_html "#{title} - #{link}"
 
   end
 
