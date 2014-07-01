46949d70acb0df2c695aaf8387c90b6748c8a76f
Gizmokid2005 <gizmokid2005@gmail.com>
2014-07-01 12:09:30 -0400

    Misc plugin - Oh...god why.

--- plugins/misc.rb
+++ plugins/misc.rb
@@ -1,10 +1,15 @@
 class Misc
   include Cinch::Plugin
 
-  match /4d3d3d3/
+  match /4d3d3d3 (.+)/, method: :c4d3d3d3
+  match /yo (.+)/, method: :cyo
 
-  def execute(m)
+  def c4d3d3d3(m)
     m.reply "#{m.user.nick}: https://www.youtube.com/watch?v=XWX4GUYGQXQ" #OhGodWhy
+  end
+
+  def cyo(m)
+    m.reply "#{m.user.nick}: Yo!" #Sigh...the things I do for you people.
   end
 
 end
