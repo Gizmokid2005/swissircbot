2a345b91625f8c9eada6b894225a8cebf545fbb1
Gizmokid2005 <gizmokid2005@gmail.com>
2014-06-30 20:56:58 -0400

    Google Plugin - visual cleanup.

--- plugins/google.rb
+++ plugins/google.rb
@@ -7,18 +7,16 @@ class Google
 
   match /google (.+)/i
 
+  def execute(m,query)
+    m.reply "#{m.user.nick}: #{search(query)}"
+  end
+
   def search(query)
 
     url = "http://www.google.com/search?q=#{CGI.escape(query)}"
-
     @res = Nokogiri.parse(open(url.to_s).read).at("h3.r")
-
     CGI.unescape_html "#{title} - #{link}"
 
-  end
-
-  def execute(m,query)
-    m.reply "#{m.user.nick}: #{search(query)}"
   end
 
   def title
@@ -31,4 +29,5 @@ class Google
                 href.split("/url?q=").last.split("&").first
               end
   end
+
 end
