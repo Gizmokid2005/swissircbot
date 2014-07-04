8be92f8860ea1c48e99870c616d4cc465332f77b
Gizmokid2005 <gizmokid2005@gmail.com>
2014-07-03 21:16:13 -0400

    Misc Plugin - Fixing my RegEx failure and making things a bit more fun.

--- plugins/misc.rb
+++ plugins/misc.rb
@@ -1,15 +1,23 @@
 class Misc
   include Cinch::Plugin
 
-  match /4d3d3d3 (.+)/, method: :c4d3d3d3
-  match /yo (.+)/, method: :cyo
+  match /4d3d3d3(?: (.+))?/, method: :c4d3d3d3
+  match /yo(?: (.+))?/, method: :cyo
 
-  def c4d3d3d3(m)
-    m.reply "#{m.user.nick}: https://www.youtube.com/watch?v=XWX4GUYGQXQ" #OhGodWhy
+  def c4d3d3d3(m, nick) #OhGodWhy
+    if nick.nil?
+      m.reply "#{m.user.nick}: https://www.youtube.com/watch?v=XWX4GUYGQXQ"
+    else
+      m.reply "#{nick}: #{m.user.nick} points you to https://www.youtube.com/watch?v=XWX4GUYGQXQ"
+    end
   end
 
-  def cyo(m)
-    m.reply "#{m.user.nick}: Yo!" #Sigh...the things I do for you people.
+  def cyo(m, nick) #Sigh...the things I do for you people.
+    if nick.nil?
+      m.reply "#{m.user.nick}: Yo!"
+    else
+      m.reply "#{nick}: #{m.user.nick} says Yo!"
+    end
   end
 
 end
