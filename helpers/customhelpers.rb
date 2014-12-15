9d45ca8f70a94c459d79bdb6b4194bb711d0ae5b
Gizmokid2005 <gizmokid2005@gmail.com>
2014-12-15 11:42:28 -0500

    CustomHelpers - Adding some comments.

--- helpers/customhelpers.rb
+++ helpers/customhelpers.rb
@@ -1,17 +1,21 @@
 module CustomHelpers
 
+  # Is the user a super-admin for the bot?
   def is_supadmin?(user)
     $superadmins.include?(user.authname.to_s) && user.authed? == true
   end
 
+  # Is the user an admin for the bot at all?
   def is_admin?(user)
     $alladmins.include?(user.authname.to_s) && user.authed? == true
   end
 
+  # Is this user an admin for this channel?
   def is_chanadmin?(channel, user)
     $adminhash[channel].include?(user.authname.to_s) && user.authed? == true
   end
 
+  # Is the bot powerful (opped)?
   def is_botpowerful?(channel)
     channel.opped?(bot)
   end
