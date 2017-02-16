47d9a884ab62d937eed1a0e146586bef3e825765
gizmokid2005 <gizmokid2005@gmail.com>
2017-02-16 11:42:20 -0800

    Define Plugin -- This plugin should now properly read/write definitions to file.

--- helpers/definehelpers.rb
+++ helpers/definehelpers.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module DefineHelpers
 
   def check_db
-    def_file = File.join($confdir, "swissircbot.yml")
+    def_file = File.join($confdir, "#{SERVER}_#{NICK}_definitions.yml")
 
     if File.exists?(def_file)
     else
@@ -16,15 +16,17 @@ module DefineHelpers
 
   def save_definition(who, term, meaning)
     df = check_db
+    defhash = Hash.new
+    defhash[term.downcase] = meaning
 
+    File.open(df, 'wb') { |f| f.write defhash.to_yaml }
   end
 
   def get_definition(term)
     term = term.downcase
-    f = YAML.load_file(check_db)
-    termdef = Array.new
-    termdef.push(f.fetch(term)['meaning'])
-    termdef.push(f.fetch(term)['who'])
+    df = YAML.load_file(check_db)
+    termdef = df.fetch(term)
+
   rescue KeyError => e
 
     return termdef
@@ -32,7 +34,15 @@ module DefineHelpers
   end
 
   def del_definition(term)
-    df = check_db
+    term = term.downcase
+    df = YAML.load_file(check_db)
+    deleted = df.delete(term)
+    if deleted
+      File.open(check_db, 'wb') { |f| f.write df.to_yaml}
+    end
+
+  rescue KeyError => e
+
   end
 
 end
