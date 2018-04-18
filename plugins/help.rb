495db362d2eefdee4914a78580cd07a2b1d03bdf
Gizmokid2005 <gizmokid2005@gmail.com>
2018-04-17 23:14:59 -0700

    Main, HelpPlugin - Update Ruby requirement to 2.4.1. Updated help plugin to properly function after a reload (help compilation used to happen only on-connect, which doesn't work for a reload).

--- plugins/help.rb
+++ plugins/help.rb
@@ -114,6 +114,7 @@ help search <query>
   EOF
 
   def allhelp(m)
+    compile_help
     response = ""
     response << @intro_message.strip << " Available plugins: "
     response << bot.config.plugins.plugins.map{ |p| format_plugin_name(p) }.join(", ")
@@ -121,14 +122,7 @@ help search <query>
     m.reply response, true
   end
 
-  # Called on startup. This method iterates the list of registered plugins
-  # and parses all their help messages, collecting them in the @help hash,
-  # which has a structure like this:
-  #
-  #   {Plugin => {"command" => "explanation"}}
-  #
-  # where +Plugin+ is the plugins class object. It also parses configuration
-  # options.
+
   def on_connect(msg)
     @help = {}
 
@@ -154,6 +148,40 @@ help search <query>
   end
 
   private
+
+  # This used to be called on startup. This method iterates the list of registered
+  # plugins and parses all their help messages, collecting them in the @help hash,
+  # which has a structure like this:
+  #
+  #   {Plugin => {"command" => "explanation"}}
+  #
+  # where +Plugin+ is the plugins class object. It also parses configuration
+  # options.
+  # This was moved here since the plugin can now be reloaded and needs to build
+  # this information after it has been reloaded and not only on connect.
+  def compile_help
+    @help = {}
+
+    if config[:intro]
+      @intro_message = config[:intro] % bot.nick
+    else
+      @intro_message = "#{bot.nick} at your service."
+    end
+
+    bot.config.plugins.plugins.each do |plugin|
+      @help[plugin] = Hash.new{|h, k| h[k] = ""}
+      next unless plugin.help # Some plugins don't provide help
+      current_command = "<unparsable content>" # For not properly formatted help strings
+
+      plugin.help.lines.each do |line|
+        if line =~ /^\s+/
+          @help[plugin][current_command] << line.strip
+        else
+          current_command = line.strip.gsub(/cinch/i, bot.name)
+        end
+      end
+    end
+  end
 
   # Format the help for a single command in a nice, unicode mannor.
   def format_command(command, explanation, plugin)
