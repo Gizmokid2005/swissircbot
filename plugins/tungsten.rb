f617c1eb914f466ed91cc0a224272e70e2368cb2
Gizmokid2005 <gizmokid2005@gmail.com>
2019-03-06 10:26:19 -0800

    TungstenPlugin - Updated for percentages.

--- plugins/tungsten.rb
+++ plugins/tungsten.rb
@@ -24,9 +24,11 @@ tungsten
           data = JSON.parse(resp.body)
           names = data.map { |p| p['owner'] }
           names.reject { |k| k == ""}.each { |n| counts[n] +=1 }
-          list = counts.sort_by {|k,v| v}.reverse.map.with_index(1) {|k,v| ["\##{v}| #{k[0]}(#{k[1]})"]}.join(", ")
+          total = names.size
+          empty = names.select {|k| k == ""}.size
+          list = counts.sort_by {|k,v| v}.reverse.map.with_index(1) {|k,v| ["\##{v}| #{k[0]}(#{(k[1].to_f / total *100).round(2)}%/#{k[1]})"]}.join(", ")
 
-          m.reply "Current Results: #{list}"
+          m.reply "Current Results: (#{(empty.to_f / total * 100).round(2)}% complete (#{total-empty}/#{total})) #{list}"
         rescue JSON::ParserError
 
           m.reply "I'm unable to parse that boss.", true
