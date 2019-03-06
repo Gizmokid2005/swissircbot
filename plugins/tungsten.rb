b8d28bbc80db7a5211aac46d973a4dc4c0c8dbe0
Gizmokid2005 <gizmokid2005@gmail.com>
2019-03-06 09:55:55 -0800

    TungstenPlugin - Added a tungsten plugin to show results from tungsten renders.

---
+++ plugins/tungsten.rb
@@ -0,0 +1,41 @@
+require 'net/http'
+require 'json'
+
+class Tungsten
+  include Cinch::Plugin
+  include CustomHelpers
+
+  set :help, <<-HELP
+tungsten 
+  This will return the ranking of tungsten renders by user.
+  HELP
+
+  match /tungsten(?: (.+))?/i
+
+  def execute(m, text)
+    if !is_blacklisted?(m.channel, m.user.nick)
+
+      counts = Hash.new(0)
+
+      uri = URI.parse("https://benedikt-bitterli.me/renderfarm/scenes/instancing/frames")
+      Net::HTTP.start(uri.host, uri.port) do |h|
+        resp = Net::HTTP.get_response(uri)
+        begin
+          data = JSON.parse(resp.body)
+          names = data.map { |p| p['owner'] }
+          names.reject { |k| k == ""}.each { |n| counts[n] +=1 }
+          list = counts.sort_by {|k,v| v}.reverse.map.with_index(1) {|k,v| ["\##{v}| #{k[0]}(#{k[1]})"]}.join(", ")
+
+          m.reply "Current Results: #{list}"
+        rescue JSON::ParserError
+
+          m.reply "I'm unable to parse that boss.", true
+        end
+
+      end
+    else
+      m.user.send BLMSG
+    end
+  end
+
+end
