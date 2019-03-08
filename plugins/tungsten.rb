bfb070e4a26dfb64b59add6607d34900849f5310
Gizmokid2005 <gizmokid2005@gmail.com>
2019-03-08 08:20:59 -0800

    TungstenPlugin - Add ability to pull stats for any scene, as well as list the scenes available.

--- plugins/tungsten.rb
+++ plugins/tungsten.rb
@@ -14,25 +14,40 @@ tungsten
 
   def execute(m, text)
     if !is_blacklisted?(m.channel, m.user.nick)
+      if text.nil?
+        uri = URI.parse("https://benedikt-bitterli.me/renderfarm/scenes")
+        Net::HTTP.start(uri.host, uri.port) do |h|
+          resp = Net::HTTP.get_response(uri)
+          begin
+            data = JSON.parse(resp.body)
+            list = data.join(", ")
 
-      counts = Hash.new(0)
+            m.reply "You need to pick one of these scenes boss: #{list}.", true
+          rescue JSON::ParserError
 
-      uri = URI.parse("https://benedikt-bitterli.me/renderfarm/scenes/yellojello/frames")
-      Net::HTTP.start(uri.host, uri.port) do |h|
-        resp = Net::HTTP.get_response(uri)
-        begin
-          data = JSON.parse(resp.body)
-          complete = data.select { |p| p['state'] == "complete"}.size
-          data.select { |p| p['state'] == "complete"}.map { |p| p['owner'] }.each { |n| counts[n] +=1 }
-          total = data.size
-          list = counts.sort_by {|k,v| v}.reverse.map.with_index(1) {|k,v| ["\##{v}|#{k[0].slice(0..2)}(#{(k[1].to_f / total *100).round(2)}%/#{k[1]})"]}.join(", ")
-
-          m.reply "Current Results: (#{(complete.to_f / total * 100).round(2)}% complete (#{complete}/#{total})) #{list}"
-        rescue JSON::ParserError
-
-          m.reply "I'm unable to parse that boss.", true
+            m.reply "I'm unable to parse that boss.", true
+          end
         end
+      else
+        counts = Hash.new(0)
 
+        uri = URI.parse("https://benedikt-bitterli.me/renderfarm/scenes/#{text}/frames")
+        Net::HTTP.start(uri.host, uri.port) do |h|
+          resp = Net::HTTP.get_response(uri)
+          begin
+            data = JSON.parse(resp.body)
+            complete = data.select { |p| p['state'] == "complete"}.size
+            data.select { |p| p['state'] == "complete"}.map { |p| p['owner'] }.each { |n| counts[n] +=1 }
+            total = data.size
+            list = counts.sort_by {|k,v| v}.reverse.map.with_index(1) {|k,v| ["\##{v}|#{k[0].slice(0..2)}(#{(k[1].to_f / total *100).round(2)}%/#{k[1]})"]}.join(", ")
+
+            m.reply "Current Results: (#{(complete.to_f / total * 100).round(2)}% complete (#{complete}/#{total})) #{list}"
+          rescue JSON::ParserError
+
+            m.reply "I'm unable to parse that boss.", true
+          end
+
+        end
       end
     else
       m.user.send BLMSG
