9a015ff9acc2c3e06c9f12433e6b10794828ec1d
Gizmokid2005 <gizmokid2005@gmail.com>
2023-08-24 17:44:04 -0400

    DicePlugin - Update regex and make this actually work....

--- plugins/dice.rb
+++ plugins/dice.rb
@@ -7,18 +7,24 @@ dice/roll <numberofdice>d<sides> [2d6 for 2 d6]
   Rolls the number of size dice. Assumes 1 if no quantity is given.
   HELP
 
-  match /(?:dice|roll)\b(\d+)?[d](\d+)/i, method: :roll
+  match /(?:dice|roll)(?=\s|$)(?: (\d+)?d(\d+))?/i, method: :roll
 
   def roll(m,dice,sides)
+    pp "My objects are dice:|#{dice}| and sides:|#{sides}|"
     if !is_blacklisted?(m.channel, m.user.nick)
-      dice = 1 unless !dice.nil?
-      total = 0
+      if sides.nil?
+        m.reply "Sorry, please specify the number of sides at least (e.g. d6).", true
+      else
+        dice = 1 unless !dice.nil?
+        total = 0
 
-      dice.to_i.times do
-        total += rand(sides.to_i) + 1
+        dice.to_i.times do
+          total += rand(sides.to_i) + 1
+        end
+
+        m.reply total, true
       end
 
-      m.reply total, true
     else
       m.user.send BLMSG
     end
